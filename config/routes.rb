d7c4c9f426912f6c5ae805d8426d6e9e84897cc0
hillsideLab <u703.fm@gmail.com>
2023-01-13 22:24:09 +0100

    Set root path.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 #   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 # end
 Rails.application.routes.draw do
+  root 'tweets#index'
   get 'tweets' => 'tweets#index'
   get 'tweets/new' => 'tweets#new'
   post 'tweets' => 'tweets#create'
